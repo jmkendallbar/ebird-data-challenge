0d284ab2411779fd6abc50cec6092b72ad1ff548
Ling-Wei Kong <47471418+lw-kong@users.noreply.github.com>
2024-05-23 23:52:53 -0400

    Update read_weather.py add sunset sunrise

--- src/read_weather.py
+++ src/read_weather.py
@@ -1,7 +1,12 @@
 import pandas as pd
-import datetime
 import os
+import datetime
 
+from astral.sun import sun
+from astral import Observer
+
+import pytz
+from timezonefinder import TimezoneFinder
 
 def get_weather_data(home_dir,selected_columns= ['datetime', 'wind_speed', 'daily_precipitation']):  
     '''
@@ -59,12 +64,54 @@ def get_weather_data(home_dir,selected_columns= ['datetime', 'wind_speed', 'dail
     
     return result_df
 
+def get_sun_data(home_dir,my_site_num,dates):
+    #file_daily_weather = os.path.join(home_dir,'swamp_aru_recorder_info.csv')
+    #daily_weather = pd.read_csv(file_daily_weather)
+    file_sensors = os.path.join(home_dir,'swamp_aru_recorder_info.csv')
+    data_sensors = pd.read_csv(file_sensors)
 
+    latitude, longitude = data_sensors.loc[data_sensors['site_num'] == my_site_num, ['latitude', 'longitude']].values[0]
+    
+    
+    # determine time zone
+    tf = TimezoneFinder()
+    timezone_str = tf.timezone_at(lat=latitude, lng=longitude)
+    if timezone_str is None:
+       print(f"Warning: Time Zone cannot be determined.")
+    tz = pytz.timezone(timezone_str)
+    
+    
+    observer = Observer(latitude, longitude)
+    sun_data_list = []
+    for date in dates:
+        sun_info = sun(observer, date=date)
+        
+        # correct the time zone
+        sun_info = {k: v.astimezone(tz) if isinstance(v, datetime.datetime) else v for k, v in sun_info.items()}
+        sun_info['date'] = date
+        sun_data_list.append(sun_info)
+
+    # change list to df
+    sun_data_df = pd.DataFrame(sun_data_list)
+    columns = ['date'] + [col for col in sun_data_df.columns if col != 'date']
+    sun_data_df = sun_data_df[columns]
+    
+    return sun_data_df
 
 
 if __name__ == "__main__":
-    home_dir = r'C:\Users\lkong\OneDrive\Documents\Research Laptop\meeting notes\2024.5 Merlin Dataset\umich_swamp_aru_anaylsis'
+    home_dir = r'..\data\umich_swamp_aru_anaylsis'
+    output_dir = os.path.join(home_dir,'outputs')
     selected_columns = ['datetime', 'wind_speed', 'daily_precipitation','air_tempp']
 
-    result_df = get_weather_data(home_dir,selected_columns)
-    print(result_df.head())
+    weather_df = get_weather_data(home_dir,selected_columns)
+    print(weather_df.head())
+    weather_df.to_csv(os.path.join(output_dir, 'weather_selected_data.csv'))
+    
+    my_site_num  = 21 
+    # these sites are so close to each other. so here I just chose a random one 
+    unique_dates = weather_df['datetime'].dt.date.unique()
+    sun_df = get_sun_data(home_dir,my_site_num,unique_dates)
+    sun_df.to_csv(os.path.join(output_dir, 'sun_data.csv'))
+    
+    
